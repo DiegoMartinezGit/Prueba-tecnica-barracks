20619971bda2559d61071f9d46da4ef7e3ec4dd3
Diego´sGit <diego.martinezp@mail.udp.cl>
2022-02-28 00:08:22 -0300

    comments in APi

--- barracks backend/api-rest.js
+++ barracks backend/api-rest.js
@@ -59,7 +59,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(device.capture());
 
-//SQL Querys
+//SQL Querys for create the base data 
 const CREATE_TABLES="create table if not exists usuarios (id serial NOT NULL ,usertype varchar(10), email varchar(255) unique, password varchar(255), device varchar(20),session varchar(255),primary key(id));";
 const USERS_RECORDS="insert into usuarios (usertype,email,password,device,session) values ('admin','admin@ejemplo.poc','admin123','DESKTOP',''),('suscriptor','suscriptor@ejemplo.poc','suscriptor123','DESKTOP','');";
 
@@ -101,18 +101,24 @@ app.put('/login',(req,res)=>{
     console.log("conectando...");
     pool.getConnection()
       .then(conn => {
+        //get user for these values of email and password
         conn.query("select * from  usuarios where usuarios.email=? and usuarios.password=?;",[req.body.email,req.body.password])
           .then((rows) => {
+            //if login is complete 
             if (rows.length > 0) {
+              //take out sensitive data 
               delete rows[0]['password'];
+              //if user already have a session destroy previus session (deny concurrent sessions).
               if(req.sessionID!==rows[0].session && rows[0].session !==''){
                 req.sessionStore.destroy(rows[0].session);
                 console.log("previus session destroyed")
 
               }
+              //update the session and current device in usage in db  
               conn.query("update usuarios set device= ? , session= ? where usuarios.id=?;",[req.device.type.toUpperCase(),req.sessionID,rows[0].id])
               .then(response=>{
                 console.log("session and device updated")
+                //setting user to the sid cookie
                 req.session.usuario=rows[0];
                 console.log(req.sessionID);
                 res.statusCode=200;
@@ -120,6 +126,7 @@ app.put('/login',(req,res)=>{
                 conn.end();
               })
             } else {
+              //incorrect password
               console.log({"not ok ":"Incorrect Username and/or Password!"});
               res.statusCode=401;
               res.json({"not ok ":"Incorrect Username and/or Password!"});
@@ -127,23 +134,27 @@ app.put('/login',(req,res)=>{
             }
           })
       }).catch(err => {
+        //db not connected
         console.log("Not conected")
         res.statusCode=503;
         res.json({"error":"no se puede conectar a bd"})
       });
 });
 app.delete('/logout',(req,res)=>{
+  //destroy current session
   req.session.destroy();
   res.statusCode=200;
   res.json({})
 });
 
 app.get('/movies', function (req, res) {
+  //if a user already have a valid cookie he is authorized to view the catalog
   if (req.session.usuario!==undefined){
     res.statusCode=200;
     res.json({"ok":"oki doki"});
   }
   else{
+    //deny authorization
     res.statusCode=401;
     res.json({"no ok ":"not oki"});
   }
@@ -151,10 +162,10 @@ app.get('/movies', function (req, res) {
   
 });
 app.put('/registerUser',(req,res)=>{
-  
   console.log("conectando...")
   pool.getConnection()
   .then(conn => {
+    // create new user 
     conn.query("insert into usuarios (usertype,email,password,device,session) values (?,?,?,?,?); ",[req.body.usertype,req.body.email,req.body.password,req.device.type.toUpperCase(),''])
       .then((db_res) => {
         console.log(db_res);
@@ -180,13 +191,14 @@ app.get('/getAllUsers',(req,res)=>{
 
   pool.getConnection().then(conn => {
       conn.query("select * from usuarios;").then((db_res) => {
-          
+          // if user already have a cookie and is admin session he is authorized to view the users info 
           if(req.session.usuario.usertype=="admin"){
             db_res.map(row=>row.password=undefined);
             res.statusCode=200;
             res.json({"usuarios": db_res});
             conn.end();
           }else{
+            //deny authorization
             res.statusCode=401;
             res.json("admin required")
             conn.end();
--- react-test-barracks/src/Apis/moviesApi.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const MOVIES_API="http://localhost:3001/"
-
-async function getMovies(){
-    const res= await axios.get(MOVIES_API);
-    console.log(res.data);
-    return res.data;
-}
